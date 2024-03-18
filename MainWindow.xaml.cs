283516f06f714356ae7a45315e9369082c9683a1
EfimBulien <isip_e.m.bulien@mpt.ru>
2024-03-18 14:18:45 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using laba1.lab1DataSetTableAdapters;
+using System.Windows;
+
+
+namespace laba1
+{
+    public partial class MainWindow : Window
+    {
+        driversTableAdapter driversTableAdapter = new driversTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DriversDataGrid.ItemsSource = driversTableAdapter.FullData();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DriversDataGrid.Columns[0].Visibility = Visibility.Collapsed;
+            DriversDataGrid.Columns[4].Visibility = Visibility.Collapsed;
+        }
+    }
+}
